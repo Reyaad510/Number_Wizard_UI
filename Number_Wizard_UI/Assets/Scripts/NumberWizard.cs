eb2b8250d0b4ca1402e3ce2f859d244cf16551af
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-11-30 01:27:28 -0500

    Brought in Previous code and refactored it to work with new Wizard UI

---
+++ Number_Wizard_UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // They are up here so they can be accessed in the Start() method and Update() method
+    // int means its a number
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+
+
+    // Start is called before the first frame update
+    // void Start used for initilization. 
+    void Start()
+    {
+        StartGame();
+    }
+
+    // void means there is no return value. Nothing is returned
+    void StartGame()
+    {
+        max = max + 1;
+        guess = (max + min) / 2;
+    }
+
+    public void onPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+    
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+       
+    }
+}
